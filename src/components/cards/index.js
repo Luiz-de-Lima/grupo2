0d19b31725b20e3bf83509531c27f3f93d33000d
renantavaress <renan_velox@hotmail.com>
2022-02-25 10:03:25 -0300

    cards

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './index.css'
 const Cards = () => {
     return (
-        
-        <div className='card-item'>
-            <div className='card-descricao'>
-                <h2>Bulbasaur</h2>
-                <p>Grass</p>
-                <p>Poison</p>
-            </div>
-            <div className='card-image'>
+        <div className='container'>
+            <div className='card-item'>
+                <div className='card-descricao'>
+                    <h2>Bulbasaur</h2>
+                    <p>Grass</p>
+                    <p>Poison</p>
+                </div>
+                <div className='card-image'>
 
+                </div>
             </div>
         </div>
 
