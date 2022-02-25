961688f913a37d8df346af60c0da6a212efcda65
renantavaress <renan_velox@hotmail.com>
2022-02-25 09:17:22 -0300

    primeiro commit

---
+++ src/components/cards/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <>
+            <h2>Bulbasaur</h2>
+            <p>Grass</p>
+            <p>Poison</p>
+        </>
+
+    )
+}
+
+export default Cards;
