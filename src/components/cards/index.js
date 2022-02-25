7e0235cf2f56a91b0f5a84d68662c081564b2875
renantavaress <renan_velox@hotmail.com>
2022-02-25 10:37:13 -0300

    cards

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,16 +4,30 @@ const Cards = () => {
     return (
         <div className='container'>
             <h1>Pokedex</h1>
-            <div className='card-item'>
-                
-                <div className='card-descricao'>
-                    <h2>Bulbasaur</h2>
-                    <p>Grass</p>
-                    <p>Poison</p>
-                </div>
-                <div className='card-image'>
+            <div className='cards'>
+                <div className='card-item'>
+                    <div className='card-descricao'>
+                        <h2>Bulbasaur</h2>
+                        <p>Grass</p>
+                        <p>Poison</p>
+                    </div>
 
+                    <div className='card-image'>
+
+                    </div>
                 </div>
+                <div className='card-item'>
+                    <div className='card-descricao'>
+                        <h2>Bulbasaur</h2>
+                        <p>Grass</p>
+                        <p>Poison</p>
+                    </div>
+
+                    <div className='card-image'>
+
+                    </div>
+                </div>
+               
             </div>
         </div>
 
